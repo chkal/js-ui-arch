4397a8cf6d5b13bb7f1f7344c76d70570ffbd44b
Christian Kaltepoth <christian@kaltepoth.de>
2015-05-26 08:22:52 +0200

    Yet another alternative modules approach

---
+++ modules2/js/model.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var FilterModel = {
+  create: function() {
+      
+    function searchImpl(query) {
+      return [ "Do some stuff here with: " + query ];
+    }
+
+    return {
+
+      query: "",
+
+      result: [ "nothing yet" ],
+
+      performSearch: function(query) {
+        this.query = query;
+        this.result = searchImpl(query);
+        bean.fire(this, 'searched', this);
+      },
+
+      on: function(event, callback) {
+        bean.on(this, event, callback);
+      }
+
+    };
+
+  };
+};
---
+++ modules2/js/view.js
@@ -0,0 +1,53 @@
+"use strict";
+
+var FilterView = {
+  create: function(model) {
+
+    /*
+     * Initialization
+     */
+
+    // instance variables
+    var model = model;
+    var $root = $('#filter');
+    var $query = $root.find('#query');
+    var $search = $root.find('#search');
+    var $result = $root.find('#result');
+
+    // element bindings
+    $search.on('click', search);
+    $query.on('keypress', submitOnEnter);
+
+    // model bindings
+    model.on('searched', render);
+
+    /*
+     * Functions
+     */
+
+    function render() {
+      $query.val(model.query);
+      $result.text(model.result);
+    }
+
+    function search() {
+      model.performSearch($query.val());
+    }
+
+    function submitOnEnter(e) {
+      if(e.keyCode === 13) {
+        e.preventDefault();
+        search();
+      }
+    }
+
+    /*
+     * Exports
+     */
+
+    return {
+      render: render
+    }
+
+  }
+};
